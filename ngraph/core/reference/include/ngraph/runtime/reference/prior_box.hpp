3a8c77e1884986a0817abcc9a312eac1dbd441cf
Efode, Irina <irina.efode@intel.com>
2020-09-14 13:35:25 +0300

    PriorBox

--- ngraph/core/reference/include/ngraph/runtime/reference/prior_box.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/prior_box.hpp
@@ -272,7 +272,8 @@ namespace ngraph
                     }
                 }
 
-                uint64_t channel_size = OH * OW;
+
+                int64_t channel_size = OH * OW;
                 if (variance.size() == 1)
                 {
                     for (uint64_t i = 0; i < channel_size; ++i)
